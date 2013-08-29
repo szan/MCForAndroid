27f1588c4ef7451e58ec0b751a99731cbaa766b2
Szymon Anusiewicz <szymon.anusiewicz@gmail.com>
2013-08-29 02:25:39 +0200

    Dodana klasa ConnectionManager

---
+++ src/com/anusiewicz/MCForAndroid/controllers/ConnectionManager.java
@@ -0,0 +1,134 @@
+package com.anusiewicz.MCForAndroid.controllers;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import com.anusiewicz.MCForAndroid.TCP.TCPClient;
+
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Szymon Anusiewicz
+ * Date: 28.08.13
+ * Time: 20:42
+ */
+public class ConnectionManager implements TCPClient.OnTCPConnectionLostListener {
+
+
+    public interface ConnectionListener {
+
+        public void onConnectionEstablished();
+
+        public void onConnectionLost();
+
+        public void onConnectionFailed();
+
+        public void onConnecting();
+
+    }
+
+    private static final String TAG = ConnectionManager.class.getName();
+    private Context context = MCForAndroidApplication.getAppContext();
+    private TCPClient mTCPClient;
+    private ArrayList<ConnectionListener> connectionListeners = new ArrayList<ConnectionListener>();
+    private ConnectionStatus status = ConnectionStatus.NOT_CONNECTED;
+
+    public static enum ConnectionStatus {
+        OK,
+        LOST,
+        FAILED,
+        NOT_CONNECTED,
+        CONNECTING
+    }
+
+    public TCPClient getTCPClient() {
+            return mTCPClient;
+    }
+
+    private void setConnectionStatus(ConnectionStatus status) {
+        this.status = status;
+    }
+
+    public ConnectionStatus getConnectionStatus() {
+        return this.status;
+    }
+
+    public void registerListener(ConnectionListener listener) {
+        connectionListeners.add(listener);
+    }
+
+    public void connectToRemoteHost(String IPAddress, String port) {
+        if (!getConnectionStatus().equals(ConnectionStatus.OK))  {
+            Log.i(TAG,"Trying to connect");
+            new ConnectTask().execute(IPAddress,port);
+        }
+    }
+
+    public void reconnectToRemoteHost(String IPAddress, String port) {
+        Log.i(TAG,"Trying to reconnect");
+        disconnectFromRemoteHost();
+        connectToRemoteHost(IPAddress,port);
+    }
+
+    public void disconnectFromRemoteHost() {
+        Log.i(TAG,"Disconnecting");
+        setConnectionStatus(ConnectionStatus.NOT_CONNECTED);
+        if (mTCPClient != null) {
+             mTCPClient.disconnect();
+             mTCPClient = null;
+        }
+
+    }
+
+    public class ConnectTask extends AsyncTask<String,Void,String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            setConnectionStatus(ConnectionStatus.CONNECTING);
+            for (ConnectionListener listener: connectionListeners){
+                listener.onConnecting();
+            }
+            try {
+                String IPAddress =params[0];
+                int Port = Integer.parseInt(params[1]);
+                mTCPClient = new TCPClient(ConnectionManager.this);
+                mTCPClient.connect(IPAddress, Port);
+                return mTCPClient.getRemoteHost();
+            } catch (Exception e) {
+                Log.e(TAG, "Couldn't connect to remote host. " + e);
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String remoteHost) {
+            super.onPostExecute(remoteHost);
+
+            if (remoteHost != null) {
+                setConnectionStatus(ConnectionStatus.OK);
+                Log.i(TAG,"Connected to: " + remoteHost);
+                for (ConnectionListener listener: connectionListeners){
+                    listener.onConnectionEstablished();
+                }
+            } else {
+                setConnectionStatus(ConnectionStatus.FAILED);
+                Log.i(TAG,"Connection failed");
+                for (ConnectionListener listener: connectionListeners){
+                    listener.onConnectionFailed();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void lostConnection() {
+        setConnectionStatus(ConnectionStatus.LOST);
+        Log.i(TAG,"Connection lost");
+        for (ConnectionListener listener: connectionListeners){
+            listener.onConnectionLost();
+        }
+    }
+
+}
---
+++ src/com/anusiewicz/MCForAndroid/controllers/MCForAndroidApplication.java
@@ -0,0 +1,30 @@
+package com.anusiewicz.MCForAndroid.controllers;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Szymon Anusiewicz
+ * Date: 28.08.13
+ * Time: 20:43
+ */
+public class MCForAndroidApplication extends Application {
+
+    private static final MCForAndroidApplication INSTANCE = new MCForAndroidApplication();
+    private static final ConnectionManager mConnectionManager = new ConnectionManager();
+    public static Context getAppContext() {
+            return INSTANCE;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //mConnectionManager = new ConnectionManager();
+
+    }
+
+    public static ConnectionManager getConnectionManager() {
+        return mConnectionManager;
+    }
+}
