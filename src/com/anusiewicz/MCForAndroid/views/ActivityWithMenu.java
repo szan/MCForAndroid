80cf50b182f2827e6ef559107009e0e94fb1e095
Szymon Anusiewicz <szymon.anusiewicz@gmail.com>
2013-09-05 22:18:15 +0200

    Dodanie menu z opcjami

---
+++ src/com/anusiewicz/MCForAndroid/views/ActivityWithMenu.java
@@ -0,0 +1,41 @@
+package com.anusiewicz.MCForAndroid.views;
+
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import com.anusiewicz.MCForAndroid.R;
+import com.anusiewicz.MCForAndroid.controllers.ConnectionManager;
+import com.anusiewicz.MCForAndroid.controllers.MCForAndroidApplication;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Szymon Anusiewicz
+ */
+public class ActivityWithMenu extends Activity {
+
+    protected ConnectionManager connectionManager = MCForAndroidApplication.getConnectionManager();
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId() == R.id.menu_item_exit){
+            connectionManager.disconnectFromRemoteHost();
+            this.finish();
+            return true;
+        } else if (item.getItemId() == R.id.menu_item_conn) {
+            connectionManager.showConnectionActivity(this);
+        }
+        return false;
+    }
+
+}
